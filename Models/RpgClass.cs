02272e532e5c48d16969846b13855e4bf9c06f60
Walter Augustine <walter.augustine10@gmail.com>
2023-06-25 18:22:11 -0400

    Ok

---
+++ Models/RpgClass.cs
@@ -0,0 +1,14 @@
+using System.Text.Json.Serialization;
+
+namespace dotnet_rpg.Models
+{
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public enum RpgClass
+    {
+        Warrior,
+        Magician,
+        Bowman,
+        Thief,
+        Pirate
+    }
+}
